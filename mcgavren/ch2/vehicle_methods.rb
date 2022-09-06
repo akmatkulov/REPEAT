81c1f7f118a081f0d8fe2e42561fd0dc044085f0
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-09-06 22:47:38 +0300

    Update vehicle_methods

--- mcgavren/ch2/vehicle_methods.rb
+++ mcgavren/ch2/vehicle_methods.rb
@@ -13,7 +13,14 @@ def use_headlights(brightness = 'low-beam')
   puts 'Watch out for deer!'
 end
 
-accelerate
-sound_horn
-use_headlights('high-beam')
-use_headlights
+def mileage(miles_driven, gas_used)
+  if gas_used.zero?
+    0.0
+  else
+    miles_driven / gas_used
+  end
+end
+
+puts mileage(400, 12)
+puts mileage(11_432, 366)
+puts mileage(22_223, 0)

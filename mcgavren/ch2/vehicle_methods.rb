c7b43813621b069476e3e03f3337453d075654b9
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-09-06 21:55:57 +0300

    Add order_soda and vehicle_methods

---
+++ mcgavren/ch2/vehicle_methods.rb
@@ -0,0 +1,19 @@
+def accelerate
+  puts 'Stepping on the gas'
+  puts 'Speeding up'
+end
+
+def sound_horn
+  puts 'Pressing the horn button'
+  puts 'Beep beep'
+end
+
+def use_headlights(brightness = 'low-beam')
+  puts "Turning on #{brightness} headlights"
+  puts 'Watch out for deer!'
+end
+
+accelerate
+sound_horn
+use_headlights('high-beam')
+use_headlights
